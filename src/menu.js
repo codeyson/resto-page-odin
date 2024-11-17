02514bcf4a30b31ff403698e40916f5e3f2880b8
Frederick Masangkay <102864294+codeyson@users.noreply.github.com>
2024-11-17 22:35:58 +0800

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,10 @@
 export default function loadMenu() {
-    const content = document.getElementById('content');
-    const menuContent = `
-        <div class="menu">
-            <h2>Our Menu</h2>
-            <p>Enjoy popcorn, drinks, and snacks while watching your favorite movies!</p>
-        </div>
-    `;
-    content.innerHTML += menuContent;
+  const content = document.getElementById('content');
+  const menuContent = `
+      <div class="menu">
+          <h2>Our Menu</h2>
+          <p>Enjoy popcorn, drinks, and snacks while watching your favorite movies!</p>
+      </div>
+  `;
+  content.innerHTML += menuContent;
 }
