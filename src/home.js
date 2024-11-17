937e4b735473c535fe2adba6802365e2e7d8d858
Frederick Masangkay <102864294+codeyson@users.noreply.github.com>
2024-11-17 22:35:45 +0800

    Update home.js

--- src/home.js
+++ src/home.js
@@ -1,12 +1,12 @@
 export default function loadHome() {
-    const content = document.getElementById('content');
-    const homeContent = `
-        <div class="video-container">
-            <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="Cinematic Video" allowfullscreen></iframe>
-        </div>
-        <div class="description">
-            A thrilling experience awaits. Dive into the world of cinema with our latest releases and timeless classics.
-        </div>
-    `;
-    content.innerHTML += homeContent;
+  const content = document.getElementById('content');
+  const homeContent = `
+      <div class="video-container">
+          <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="Cinematic Video" allowfullscreen></iframe>
+      </div>
+      <div class="description">
+          A thrilling experience awaits. Dive into the world of cinema with our latest releases and timeless classics.
+      </div>
+  `;
+  content.innerHTML += homeContent;
 }
