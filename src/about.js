e1db6a9a5060ffe75d2bac4e08ebd032776c2fdd
Frederick Masangkay <102864294+codeyson@users.noreply.github.com>
2024-11-17 22:36:10 +0800

    Update about.js

--- src/about.js
+++ src/about.js
@@ -1,10 +1,10 @@
 export default function loadAbout() {
-    const content = document.getElementById('content');
-    const aboutContent = `
-        <div class="about">
-            <h2>About CineView</h2>
-            <p>Welcome to CineView, the ultimate platform for movie enthusiasts. Explore, enjoy, and indulge in the cinematic universe.</p>
-        </div>
-    `;
-    content.innerHTML += aboutContent;
+  const content = document.getElementById('content');
+  const aboutContent = `
+      <div class="about">
+          <h2>About CineView</h2>
+          <p>Welcome to CineView, the ultimate platform for movie enthusiasts. Explore, enjoy, and indulge in the cinematic universe.</p>
+      </div>
+  `;
+  content.innerHTML += aboutContent;
 }
